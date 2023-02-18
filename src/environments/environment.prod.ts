2035019940159c4b0a1bb43a5488b0c804e83db6
mcknight816 <mcknight816@gmail.com>
2023-02-17 21:21:21 -0500

    set correct endpoints

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api:"https://bizvest-service-pqjwqny6pq-uc.a.run.app",
+  api:"/bizvest-service",
   saasy:"https://saasy-service.bluntsoftware.com/api/v1",
   appId:"6382622268f6bb28b8b8ade2"
 };
